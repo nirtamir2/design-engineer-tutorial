18d4bbf94a5fd162ab506f27e4f88a01c04546f8
Nir Tamir <nirtamir2@gmail.com>
2024-09-13 21:15:51 +0300

    start content

--- src/content/tutorial/1-basics/1-introduction/1-welcome/_files/MyApp.tsx
+++ src/content/tutorial/1-basics/1-introduction/1-welcome/_files/MyApp.tsx
@@ -1,3 +1,19 @@
+import imageBackgroud from "./snow-board.jpg";
+
 export default function MyApp() {
-  return <div>MyApp</div>;
+  return <div className="flex h-full flex-col">
+    <h1 className="text-5xl pt-16 px-8">It's going to be a good day</h1>
+    <button className="flex-1" type="button">start your day</button>
+    <div className="absolute size-full -z-10">
+    <img
+        src={imageBackgroud}
+        alt="snow board"
+        className="size-full object-cover"
+    />
+    </div>
+    <div className="flex gap-2 py-12 px-8">
+    <span className="text-5xl">G</span>
+    <span>ood morning Time to start another beautiful day</span>
+    </div>
+  </div>;
 }
